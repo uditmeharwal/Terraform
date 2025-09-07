6d542800bd0c9fda2ad8c27887904ab427481652
uditmeharwal <uditmeharwal2@gmail.com>
2025-09-07 13:08:17 +0530

    s3 bucket_with random_id generator

--- S3/main.tf
+++ S3/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "6.12.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
   }
 }
 
@@ -16,22 +20,26 @@ output "s3" {
 
 }
 
+resource "random_bytes" "byte" {
+    length = 8
+  
+}
+
 resource "aws_s3_bucket" "bucket" {
-  bucket = "uditmeharwal-terraform-bucket"
+  bucket = "uditmeharwal-${random_bytes.byte.hex}"
 
   tags = {
-    Name        = "uditmeharwal-terraform_bucket"
+    Name = "bucket"
   }
   
 }
 
 # Upload a local file to an existing S3 bucket
 resource "aws_s3_object" "file_to_upload" {
-  bucket       = "uditmeharwal-terraform-bucket"
+  bucket       = aws_s3_bucket.bucket.bucket  # name of the bucket"
   key          = "jaat.txt"     # object name in S3
   source       = "jaat.txt"     # path to your local file
   content_type = "text/plain"
 
-  # Helps Terraform detect changes to the local file and re-upload
-  etag = filemd5("jaat.txt")
+  
 }
